169a2fc1df254b3f2167526e1b5577cd7fceb603
JairoAndresBoada <jairo.boada@ceiba.com.co>
2018-01-18 16:19:00 -0500

    Se agrega cambio en pruebas unnitarias

--- src/test/java/co/ceiba/parqueadero/ParqueaderoCeiba/ParqueaderoCeibaApplicationTests.java
+++ src/test/java/co/ceiba/parqueadero/ParqueaderoCeiba/ParqueaderoCeibaApplicationTests.java
@@ -1,17 +1,6 @@
 package co.ceiba.parqueadero.ParqueaderoCeiba;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
 public class ParqueaderoCeibaApplicationTests {
-	
-	
-	@Test
-	public void calculartest()  {
-		
-	}
 
 }
