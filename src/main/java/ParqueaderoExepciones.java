494da02962013bd801466f0159c8fd9c49239e46
JairoAndresBoada <jairo.boada@ceiba.com.co>
2018-01-18 13:06:59 -0500

    Se crea una prueba unitaria para realizar la configuracionn Test en el jenkins.

---
+++ src/main/java/ParqueaderoExepciones.java
@@ -0,0 +1,17 @@
+
+
+
+public class ParqueaderoExepciones extends Exception{
+
+	
+	private static final long serialVersionUID = -7208854632765744150L;
+	
+	public ParqueaderoExepciones() {
+		super();
+	}
+	
+	public ParqueaderoExepciones (String message) {
+		super(message);
+	}
+
+}
--- src/main/java/co/ceiba/parqueadero/modelo/Parqueadero.java
+++ src/main/java/co/ceiba/parqueadero/modelo/Parqueadero.java
@@ -1,5 +1,38 @@
 package co.ceiba.parqueadero.modelo;
 
-public class Parqueadero {
+import java.io.Serializable;
+import java.util.Calendar;
 
+public class Parqueadero implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	private int id;
+	private Vehiculo vehiculo;
+	private Calendar fechaIngreso;
+	private Calendar fechaSalida;
+	
+	public Parqueadero(Vehiculo vehiculo, Calendar fechaIngreso) {
+		super();
+		this.vehiculo = vehiculo;
+		this.fechaIngreso = fechaIngreso;
+	}
+	
+	public Calendar getFechaSalida() {
+		return fechaSalida;
+	}
+
+	public void setFechaSalida(Calendar fechaSalida) {
+		this.fechaSalida = fechaSalida;
+	}
+
+	public Vehiculo getVehiculo() {
+		return vehiculo;
+	}
+
+	public Calendar getFechaIngreso() {
+		return fechaIngreso;
+	}
+	
 }
