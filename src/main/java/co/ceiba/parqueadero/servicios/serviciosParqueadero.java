7a31617bb36aaba422a1fa6a59fa536c9289f34a
JairoAndresBoada <jairo.boada@ceiba.com.co>
2018-01-19 14:14:59 -0500

    Se agrega la implemnetacion del la logica de los servicios del parqueadero en esta logica se coloca cuando ingresa el vehiculo al parqueadero la salida del vehiculo y obtiene los vehiculos que estan dentro del parqueadero.,

--- src/main/java/co/ceiba/parqueadero/servicios/serviciosParqueadero.java
+++ src/main/java/co/ceiba/parqueadero/servicios/serviciosParqueadero.java
@@ -1,5 +1,13 @@
 package co.ceiba.parqueadero.servicios;
 
+import java.util.List;
+import co.ceiba.parqueadero.exepciones.ParqueaderoExcepsiones;
+import co.ceiba.parqueadero.exepciones.ParqueaderoServiciosExcepsiones;
+import co.ceiba.parqueadero.modelo.Parqueadero;
+
 public interface serviciosParqueadero {
+	boolean ingresarVehiculoParqueadero(String placa, int cilindraje) throws ParqueaderoServiciosExcepsiones;
+	double salidaVehiculoParqueadero(String placa) throws ParqueaderoServiciosExcepsiones;
+	List<Parqueadero> obtenerVehiculos() throws ParqueaderoExcepsiones;
 
 }
--- src/main/java/co/ceiba/parqueadero/servicios/serviciosParqueaderoLogica.java
+++ src/main/java/co/ceiba/parqueadero/servicios/serviciosParqueaderoLogica.java
@@ -1,5 +1,62 @@
 package co.ceiba.parqueadero.servicios;
 
-public class serviciosParqueaderoLogica {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import co.ceiba.parqueadero.exepciones.ParqueaderoExcepsiones;
+import co.ceiba.parqueadero.exepciones.ParqueaderoLogicaExcepsiones;
+import co.ceiba.parqueadero.exepciones.ParqueaderoServiciosExcepsiones;
+import co.ceiba.parqueadero.logicaNegocio.NegocioLogica;
+import co.ceiba.parqueadero.modelo.Parqueadero;
+import co.ceiba.parqueadero.repositorioLogica.repositorioParqueadero;
+import co.ceiba.parqueadero.valoresfijos.ValoresFijos;
+
+public class serviciosParqueaderoLogica implements serviciosParqueadero {
+	
+	
+	@Autowired
+	NegocioLogica parqueaderoLogica;
+	
+	@Autowired
+	repositorioParqueadero parqueaderorepository;
+
+	@Override
+	public boolean ingresarVehiculoParqueadero(String placa, int cilindraje) throws ParqueaderoServiciosExcepsiones {
+		if(placa.isEmpty()) {
+			throw new ParqueaderoServiciosExcepsiones(ValoresFijos.PLACA_VACIA);
+		}
+		if(!placa.matches(ValoresFijos.PATRON_PLACA)) {
+			throw new ParqueaderoServiciosExcepsiones(ValoresFijos.PLACA_INVALIDA);
+		}
+		if(cilindraje <0) {
+			throw new ParqueaderoServiciosExcepsiones(ValoresFijos.CILINDRAJE_INVALIDO);
+		}
+		try {
+			return parqueaderoLogica.ingresarVehiculo(placa, cilindraje);
+		} catch (ParqueaderoLogicaExcepsiones e) {
+			throw new ParqueaderoServiciosExcepsiones(ValoresFijos.ERROR_REGISTRO,e);
+		}
+	}
+
+	@Override
+	public double salidaVehiculoParqueadero(String placa) throws ParqueaderoServiciosExcepsiones {
+		if(placa.isEmpty()) {
+			throw new ParqueaderoServiciosExcepsiones(ValoresFijos.PLACA_VACIA);
+		}
+		if(!placa.matches(ValoresFijos.PATRON_PLACA)) {
+			throw new ParqueaderoServiciosExcepsiones(ValoresFijos.PLACA_INVALIDA);
+		}
+		try {
+			return parqueaderoLogica.salidaParqueadero(placa);
+		} catch (ParqueaderoLogicaExcepsiones e) {
+			throw new ParqueaderoServiciosExcepsiones(ValoresFijos.ERROR_SALIDA_VEHICULO,e);
+		}
+	}
+
+	@Override
+	public List<Parqueadero> obtenerVehiculos() throws ParqueaderoExcepsiones {
+		return parqueaderorepository.obtenerVehiculos();
+	}
 
 }
